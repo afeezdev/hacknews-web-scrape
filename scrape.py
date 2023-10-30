6f1b790d52d43e80e17eb00ed89c6f3c82d4be44
afeezdev <afeezdev@gmail.com>
2023-10-30 21:09:42 +0000

    hackernews 1

--- scrape.py
+++ scrape.py
@@ -3,12 +3,17 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/')
 soup = BeautifulSoup(res.text, 'html.parser')
-# using css selector
-# print(soup.select('.score'))
-# print(soup.select('#score_38043033'))
 links = soup.select('.titleline')
 votes = soup.select('.score')
-# print(votes[0].get('id'))
-# print(votes[0].contents[0])
 
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None )
+        points = int(votes[idx].getText().replace(' points', ''))
+        print(points)
+        hn.append({'title': title, 'link': href})
+    return hn
 
+create_custom_hn(links, votes)

47d594be76f7e8e4e070161723dc953e9a2e0c7f
afeezdev <afeezdev@gmail.com>
2023-10-31 20:52:13 +0000

    hackernews 3

--- scrape.py
+++ scrape.py
@@ -3,9 +3,16 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get('https://news.ycombinator.com/')
+res2 = requests.get('https://news.ycombinator.com/')
 soup = BeautifulSoup(res.text, 'html.parser')
-links = soup.select('.titleline')
-subtext = soup.select('.subtext')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+links1 = soup.select('.titleline')
+subtext1 = soup.select('.subtext')
+links2 = soup2.select('.titleline')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links1 + links2
+mega_subtext = subtext1 + subtext2
 
 def create_custom_hn(links, subtext):
     hn = []
@@ -20,4 +27,4 @@ def create_custom_hn(links, subtext):
                 hn.append({'title': title, 'link': href, 'votes': points})
     return hn
 
-pprint.pprint(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
